a041dcb7155352a5563a13a583529f00b3e77b52
Lehinskiy05 <peskovatskiy.leha@mail.ru>
2024-09-30 22:43:45 +0500

    Corrected new functions

--- js/functions.js
+++ js/functions.js
@@ -8,11 +8,39 @@ function isPalindrome(string) {
 }
 
 function getNumberFromString(string) {
-  let digits = Array();
+  string = String(string);
+  const digits = Array();
   for (let i = 0; i < string.length; i++) {
     if ('0' <= string[i] && string[i] <= '9') {
       digits.push(string[i]);
     }
   }
-  return parseInt(digits.join(''));
+  return parseInt(digits.join(''), 10);
 }
+
+
+function AssertThat(assertion) {
+  if (!assertion) {
+    throw new Error('Assertion failed');
+  }
+  return true;
+}
+
+// Test cases
+AssertThat(checkStringLength('проверяемая строка', 20) === true);
+AssertThat(checkStringLength('проверяемая строка', 18) === true);
+AssertThat(checkStringLength('проверяемая строка', 10) === false);
+
+AssertThat(isPalindrome('топот') === true);
+AssertThat(isPalindrome('ДовОд') === true);
+AssertThat(isPalindrome('Кекс') === false);
+AssertThat(isPalindrome('Лёша на полке клопа нашёл ') === true);
+
+AssertThat(getNumberFromString('2023 год') === 2023);
+AssertThat(getNumberFromString('ECMAScript 2022') === 2022);
+AssertThat(getNumberFromString('1 кефир, 0.5 батона') === 105);
+AssertThat(getNumberFromString('агент 007') === 7);
+AssertThat(isNaN(getNumberFromString('а я томат')));
+AssertThat(getNumberFromString(2023) === 2023);
+AssertThat(getNumberFromString(-1) === 1);
+AssertThat(getNumberFromString(1.5) === 15);
